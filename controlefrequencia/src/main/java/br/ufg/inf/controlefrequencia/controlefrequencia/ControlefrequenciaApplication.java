be60d3ecb58093b042308ca4f88b5aa3061e5ec9
Saulo Calixto <sauloacalixto@gmail.com>
2019-05-22 19:26:31 -0300

    subsituindo arquivos do projeto

--- controlefrequencia/src/main/java/br/ufg/inf/controlefrequencia/controlefrequencia/ControlefrequenciaApplication.java
+++ controlefrequencia/src/main/java/br/ufg/inf/controlefrequencia/controlefrequencia/ControlefrequenciaApplication.java
@@ -1,13 +1,13 @@
-package br.ufg.inf.frequencia;
+package br.ufg.inf.controlefrequencia.controlefrequencia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrequenciaApplication {
+public class ControlefrequenciaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrequenciaApplication.class, args);
+		SpringApplication.run(ControlefrequenciaApplication.class, args);
 	}
 
 }
--- controlefrequencia/src/test/java/br/ufg/inf/controlefrequencia/controlefrequencia/ControlefrequenciaApplicationTests.java
+++ controlefrequencia/src/test/java/br/ufg/inf/controlefrequencia/controlefrequencia/ControlefrequenciaApplicationTests.java
@@ -1,4 +1,4 @@
-package br.ufg.inf.frequencia;
+package br.ufg.inf.controlefrequencia.controlefrequencia;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FrequenciaApplicationTests {
+public class ControlefrequenciaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test;
+//	public void contextLoads() {
+//	}
 
 }
