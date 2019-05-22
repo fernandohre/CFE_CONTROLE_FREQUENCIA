2e1d0778821f3c650dc5ac32c14d4b4453b77d6a
Fernando Henrique Quicano Callata <fernando.callata@lggo.com.br>
2019-05-22 13:23:41 -0300

    adicionando projeto previamente configurado com springBoot

---
+++ sistema/src/main/java/br/ufg/inf/frequencia/FrequenciaApplication.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.frequencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrequenciaApplication.class, args);
+	}
+
+}
